4cb6f988a85bed47492570b3729420d662da9869
haskio <haskio@msn.com>
2015-07-24 19:47:06 +0800

    update

--- js/fancy-box.js
+++ js/fancy-box.js
@@ -7,9 +7,6 @@ $(document).ready(function() {
       $imageWrapLink = $image.wrap('<a href="' + this.getAttribute('src') + '"></a>').parent('a');
     }
     $imageWrapLink.addClass('fancybox');
-    if(this.title){
-      $imageWrapLink.attr("title",this.title); //make sure img title tag will show correctly in fancybox
-    }
   });
 });
 $('.fancybox').fancybox({
